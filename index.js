6d7e520c2c6666bc497250029ae386bd8282935d
Abena Adu <abeanadu@gmail.com>
2022-09-29 14:17:09 -0400

    commit tree 9c14fb1a89e38475d687a303fa91c692dead8672

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+function appendCat(name) {
+    const allCats = [...cats, name];
+    return allCats;
+}
+function prependCat(name) {
+    const allCats = [name, ...cats];
+    return allCats;
+}
+function removeLastCat(name) {
+    return cats.slice(0, -1);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
